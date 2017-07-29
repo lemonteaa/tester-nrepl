a969a55c956a0ea134a06fef7327f0849fa3e42d
lemontea <lemontea.Tom@gmail.com>
2017-07-29 22:50:35 +0800

    Can trigger test running

--- src/leiningen/tester_nrepl.clj
+++ src/leiningen/tester_nrepl.clj
@@ -9,8 +9,12 @@
   [project & args]
   (println "Hi!"))
 
+(def ^:dynamic cur-transport nil)
+(def ^:dynamic cur-msg nil)
+
 (defn pass
   [transport msg m]
+  (println "Enter: pass")
   (println m)
   (u/answer transport msg { :type :test-result
                             :fact 123
@@ -31,17 +35,36 @@
     (throw (ex-info "Unknown failure type" { :input-arg m }))))
 
 (def emission-map (assoc default/emission-map
-                         :fail fail))
+                         :pass (partial pass cur-transport cur-msg)))
 
 (state/install-emission-map emission-map)
+
+(midje.config/with-augmented-config { :emitter 'leiningen.tester-nrepl }
+  (let [a 'foo.core-test]
+      (midje/load-facts a)))
 
 (defn wrap-tester
   [h]
   (fn [{:keys [op transport ns-test] :as msg}]
     (if (= "test-midje" op)
-      (do (midje/load-facts ns-test))
+      (binding [cur-transport transport
+                cur-msg msg]
+        (println cur-transport cur-msg)
+        (println ns-test)
+        (midje/load-facts (symbol ns-test)))
       (h msg))))
 
-(midje/load-facts 'foo.core-test)
+(let [a 'foo.core-test]
+(midje/load-facts a))
+(midje/load-facts (symbol "foo.core-test"))
+(symbol "foo.core-test")
 (midje/forget-facts :all)
 (midje/check-facts :all)
+
+(defn tt [{:keys [hi]}]
+  hi)
+
+(let [b 'joo]
+;(= (tt { :hi 'joo}) `'~b))
+  `'~b)
+
--- test/leiningen/tester_nrepl/plugin_test.clj
+++ test/leiningen/tester_nrepl/plugin_test.clj
@@ -1,12 +1,15 @@
 (ns leiningen.tester-nrepl.plugin-test
   (:require [clojure.tools.nrepl :as repl]
-            [clojure.tools.nrepl.server :as server]))
+            [clojure.tools.nrepl.server :as server]
+            [leiningen.tester-nrepl :refer [wrap-tester]]))
 
-(with-open [tserver (server/start-server :port 3456)
+(with-open [tserver (server/start-server :port 3456 :handler (server/default-handler #'wrap-tester))
             conn (repl/connect :port 3456)]
   (-> (repl/client conn 1000)
-      (repl/message {:op :eval :code "(+ 1 1)"})
-       repl/response-values))
+      ;(repl/message {:op :eval :code "(+ 1 1)"})
+      (repl/message {:op "test-midje" :ns-test 'foo.core-test })
+      repl/response-values))
 
 
-;server/default-middlewares
+;(into [#'wrap-tester] server/default-middlewares)
+;(server/default-handler #'wrap-tester)
